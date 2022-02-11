0734c6133886ac55064022544d38fff162c97ee0
Devon Ryan <dpryan79@gmail.com>
2022-02-11 23:57:22 +0100

    us precompiled binaries

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,10 @@ set -x
 # https://github.com/conda-forge/llvmdev-feedstock/issues/54
 #rm -rf $BUILD_PREFIX/lib/libLLVM*.a $BUILD_PREFIX/lib/libclang*.a
 
-mamba install -y ldc=1.17 -p ${BUILD_PREFIX}
-find ${BUILD_PREFIX} -name ldmd2
+#mamba install -y ldc -p ${BUILD_PREFIX}
+#find ${BUILD_PREFIX} -name ldmd2
+curl -fsS https://dlang.org/install.sh | bash -s ldc
+find ~/ -name ldmd2
 
 # Build latest version
 mkdir build
@@ -17,7 +19,7 @@ cmake -G Ninja \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DBUILD_SHARED_LIBS=ON \
-      -DD_COMPILER=${BUILD_PREFIX}/bin/ldmd2 \
+      -DD_COMPILER=~/bin/ldmd2 \
       ..
 ninja install
 ldc2 -version
