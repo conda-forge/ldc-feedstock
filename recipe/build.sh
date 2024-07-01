535559824a46e702b448d74de629f5c11ef2cfca
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-02 00:41:23 +0200

    Use latest version of ldc2 that works with glibc 2.17

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,10 @@ set -x
 
 # In the future we can just use mamba install to get a previous version on all platforms
 if [[ "${target_platform}" == "linux-aarch64" ]]; then
-    curl -fsS https://dlang.org/install.sh | bash -s ldc
-    source ~/dlang/ldc-1.32.0/activate
-    ldc2
+    LDC_VERSION=1.26.0 # Latest version that works with glibc 2.17
+    curl -fsS https://dlang.org/install.sh | bash -s ldc-$LDC_VERSION
+    source ~/dlang/ldc-$LDC_VERSION/activate
+    ldc2 -version
     DCMP=ldmd2
 else
     mamba install -y ldc -p ${BUILD_PREFIX}
