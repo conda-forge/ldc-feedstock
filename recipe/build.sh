dd02c49898bab92f889df3156bb9156967a79ca3
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-02 00:36:35 +0200

    Try older version of ldc

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,25 +4,26 @@ set -x
 
 # In the future we can just use mamba install to get a previous version on all platforms
 if [[ "${target_platform}" == "linux-aarch64" ]]; then
-   curl -fsS https://dlang.org/install.sh | bash -s ldc
-   source ~/dlang/ldc-1.38.0/activate
-   DCMP=ldmd2
+    curl -fsS https://dlang.org/install.sh | bash -s ldc
+    source ~/dlang/ldc-1.32.0/activate
+    ldc2
+    DCMP=ldmd2
 else
-   mamba install -y ldc -p ${BUILD_PREFIX}
-   DCMP=${BUILD_PREFIX}/bin/ldmd2
+    mamba install -y ldc -p ${BUILD_PREFIX}
+    DCMP=${BUILD_PREFIX}/bin/ldmd2
 fi
 
 # Build latest version
 mkdir build
 cd build
 cmake -G Ninja \
-      ${CMAKE_ARGS} \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DBUILD_SHARED_LIBS=ON \
-      -DD_COMPILER=$DCMP \
-      ..
+    ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DBUILD_SHARED_LIBS=ON \
+    -DD_COMPILER=$DCMP \
+    ..
 ninja install
 ldc2 -version
 
@@ -36,12 +37,12 @@ fi
 mkdir build
 cd build
 cmake -G Ninja \
-      ${CMAKE_ARGS} \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DBUILD_SHARED_LIBS=ON \
-      -DD_COMPILER=${PREFIX}/bin/ldmd2 \
-      ..
+    ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DBUILD_SHARED_LIBS=ON \
+    -DD_COMPILER=${PREFIX}/bin/ldmd2 \
+    ..
 ninja install
 ldc2 -version
